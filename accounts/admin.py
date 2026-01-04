8b71f499e47ac90521c607e5cfabdeb44d3c73b9
mohsen <amirimohsen72@gmail.com>
2026-01-05 01:25:43 +0330

    #10 fix jalali date for customer birth date

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserChangeForm,CustomUserCreationForm
 from .models import CustomUser , Customer
-# Register your models here.
+from jalali_date.admin import ModelAdminJalaliMixin
 
 
 class CustomUserAdmin(UserAdmin):
@@ -23,7 +23,7 @@ admin.site.register(CustomUser,CustomUserAdmin)
 
 
 @admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
+class CustomerAdmin(ModelAdminJalaliMixin,admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'phone_number', 'is_active','created_at')
     list_filter = ('is_active',)
     search_fields = ('phone_number','last_name', 'first_name',)
--- config/settings.py
+++ config/settings.py
@@ -148,24 +148,19 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # LOGOUT_REDIRECT_URL = 'home'
 
 JALALI_DATE_DEFAULTS = {
+   # if change it to true then all dates of the list_display will convert to the Jalali.
+   'LIST_DISPLAY_AUTO_CONVERT': True,
    'Strftime': {
         'date': '%y/%m/%d',
         'datetime': '%H:%M:%S _ %y/%m/%d',
     },
     'Static': {
         'js': [
-            # loading datepicker
             'admin/js/django_jalali.min.js',
-            # OR
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
-            # 'admin/js/main.js',
         ],
         'css': {
             'all': [
-                'admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css',
+              'admin/css/django_jalali.min.css',
             ]
         }
     },
