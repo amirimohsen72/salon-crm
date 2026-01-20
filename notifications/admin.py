a25911516a10ca91db5fb91e3f7d737c821ad6f2
mohsen <amirimohsen72@gmail.com>
2026-01-20 16:32:12 +0330

    #16 json data insert
    python manage.py makemigrations notifications --empty
    
    python manage.py migrate

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import SMSTemplate, SMSLog
+from jalali_date.admin import ModelAdminJalaliMixin
 
 
 @admin.register(SMSTemplate)
-class SMSTemplateAdmin(admin.ModelAdmin):
+class SMSTemplateAdmin(ModelAdminJalaliMixin,admin.ModelAdmin):
     list_display = ('id', 'title', 'type', 'is_active', 'updated_at')
     list_filter = ('type', 'is_active')
     search_fields = ('title', 'message')
@@ -11,7 +12,7 @@ class SMSTemplateAdmin(admin.ModelAdmin):
 
 
 @admin.register(SMSLog)
-class SMSLogAdmin(admin.ModelAdmin):
+class SMSLogAdmin(ModelAdminJalaliMixin,admin.ModelAdmin):
     list_display = ('id', 'phone_number', 'customer', 'appointment', 'status', 'sent_at', 'created_at')
     list_filter = ('status', 'template', 'created_at')
     search_fields = ('phone_number', 'message', 'customer__first_name', 'customer__last_name')
