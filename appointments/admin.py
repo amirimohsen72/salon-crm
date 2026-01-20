ce4018a1935f03dc89724645a35dbb354ab78fc5
mohsen <amirimohsen72@gmail.com>
2026-01-20 15:47:35 +0330

    #14 اصلاح رزرو وقت

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,18 +1,31 @@
 from django.contrib import admin
 from .models import Appointment, AppointmentService
+from jalali_date.admin import ModelAdminJalaliMixin
+from django.utils.translation import gettext as _
+from django.utils.timezone import localtime
+from jalali_date import jdatetime
 
 
-class AppointmentServiceInline(admin.TabularInline):
+class AppointmentServiceInline(ModelAdminJalaliMixin,admin.TabularInline):
     model = AppointmentService
     extra = 1
     autocomplete_fields = ['service']
-    fields = ('service', 'start_time', 'end_time')
-    readonly_fields = ('end_time',)
+    fields = ('service', 'start_time', 'end_time_jalali')
+    readonly_fields = ('end_time_jalali',)
 
+    def end_time_jalali(self, obj):
+        if not obj.end_time:
+            return "-"
+        dt = localtime(obj.end_time)
+        return jdatetime.datetime.fromgregorian(datetime=dt).strftime('%Y/%m/%d %H:%M')
+
+    end_time_jalali.short_description = _('end time')
 
 @admin.register(Appointment)
-class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'status', 'created_at')
-    list_filter = ('status',)
+class AppointmentAdmin(ModelAdminJalaliMixin,admin.ModelAdmin):
+    list_display = ('id', 'customer', 'status', 'reservation_date', 'created_at')
+    list_filter = ('status','reservation_date')
     search_fields = ('customer__first_name', 'customer__last_name', 'customer__phone_number')
     inlines = [AppointmentServiceInline]
+    date_hierarchy = 'reservation_date'
+
--- appointments/models.py
+++ appointments/models.py
@@ -16,30 +16,42 @@ class Appointment(models.Model):
         verbose_name = _('appointment')
         verbose_name_plural = _('appointments')
 
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE,verbose_name=_('customer') )
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name=_('created at'))
+    reservation_date = models.DateField(
+        null=True,
+        verbose_name=_('reservation date'),
+        db_index=True
+    )
+    note = models.TextField(
+        blank=True,
+        verbose_name=_('note')
+    )
     status = models.CharField(
         max_length=20,
         choices=Status.choices,
-        default=Status.RESERVED
+        default=Status.RESERVED,
+        verbose_name=_('status')
     )
 
     def __str__(self):
-        return f"{self.customer} ({self.id})"
+        return f"{self.customer} ({self.reservation_date})"
     
 
 class AppointmentService(models.Model):
     appointment = models.ForeignKey(
         Appointment,
         on_delete=models.CASCADE,
-        related_name='services'
+        related_name='services',
+        verbose_name=_('appointment')
     )
     service = models.ForeignKey(
         Service,
-        on_delete=models.PROTECT
+        on_delete=models.PROTECT,
+        verbose_name=_('service')
     )
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField(blank=True, null=True)
+    start_time = models.DateTimeField(verbose_name=_('start time'))
+    end_time = models.DateTimeField(blank=True, null=True, verbose_name=_('end time'))
 
     def save(self, *args, **kwargs):
         if self.start_time:
